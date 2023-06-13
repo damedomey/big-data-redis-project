23198c4e454e611250fddb80ea97f3027f12ff90
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2023-06-13 10:32:41 +0200

    fix: operation date

--- src/test/java/org/unice/OperationTest.java
+++ src/test/java/org/unice/OperationTest.java
@@ -2,7 +2,6 @@ package org.unice;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import junit.framework.TestCase;
 import org.unice.model.Operation;
@@ -11,16 +10,12 @@ import org.unice.service.OperationService;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class OperationTest extends TestCase{
     
     public void testCreateOperation(){
         ObjectMapper objectMapper = new ObjectMapper();
-        // objectMapper.registerModule(new JavaTimeModule());
-        // SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-        // objectMapper.setDateFormat(df);
         try {
             File file = Paths.get("src", "main", "resources", "operations.json").toFile();
             List<Operation> operations = objectMapper
